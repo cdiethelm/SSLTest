ed99dd661b44d20907e81833f9817622a5522e8d
Christoph Diethelm <c.diethelm@diethelm-aufzuege.ch>
2019-01-30 18:53:38 +0100

    Added go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang.org/x/crypto/acme/autocert
+
+// go: no requirements found in vendor/vendor.json
--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
-	"log"
 	"net/http"
+
+	"golang.org/x/crypto/acme/autocert"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love git!")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":80", nil))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello Secure World")
+	})
+
+	certManager := autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		Cache:  autocert.DirCache("certs"),
+	}
+
+	server := &http.Server{
+		Addr:    ":443",
+		Handler: mux,
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
+	}
+
+	go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
+	server.ListenAndServeTLS("", "")
 }
