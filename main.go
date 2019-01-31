393762ce5a6636395211751cbd85896a027330f3
Christoph Diethelm <c.diethelm@diethelm-aufzuege.ch>
2019-01-31 09:47:12 +0100

    Changed port to os.genenv

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello Secure World")
@@ -28,5 +32,5 @@ func main() {
 		go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
 		server.ListenAndServeTLS("", "")
 	*/
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":"+port, mux)
 }
